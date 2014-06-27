03ee758672c2c761ffcba468805a7fc3d03f436f
Jonathan Grisez <jonathan.grisez@protime.be>
2014-06-27 14:49:59 +0200

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syfuun::Application.config.secret_token = 'b6459a9a7ddb71df38c182fb97b28eb47db9435c8d1d0b69419f893378e8b729431f3cf796a312a4395442a025dec5d2ee7ac1d07cf092d812d09ecfd327b69c'
