39dae5c9e38934180b04bec06cf0e585277659a1
Jonathan Grisez <jonathan.grisez@protime.be>
2014-07-01 13:45:34 +0200

    list: provided model test, extended functional test (+ view), fixed failing tests.

--- test/functional/todos_controller_test.rb
+++ test/functional/todos_controller_test.rb
@@ -5,6 +5,21 @@ class TodosControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_template :index
-    assert_not_nil assigns(:todos)
+
+    todo_titles = assigns(:todos).map(&:title)
+
+    assert_equal(['Make sure TDD is used', 'Make sure to work outside in'], todo_titles)
+  end
+
+  test '#index view should show all todos' do#for discussion
+    get :index
+
+    expected_list = ['Make sure TDD is used', 'Make sure to work outside in']
+    assert_select('ul li') do |elements|
+      assert_equal(expected_list.length, elements.length)
+      (0...expected_list.length).each do |index|
+        assert_include(elements[index].to_s, expected_list[index])
+      end
+    end
   end
 end
--- test/unit/todo_test.rb
+++ test/unit/todo_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
+  test '#title is an attribute' do
+    #for the sake of having a unit test as demonstration
+    todo = Todo.new
+    assert_nil todo.title
+    todo.title = 'some random todo item'
+    assert_equal(todo.title, 'some random todo item')
+  end
 end
